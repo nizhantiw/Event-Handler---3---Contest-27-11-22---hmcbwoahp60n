6dfb15f11d08b4f20416f490fc25c601888aa0cc
nizhantiw <nizhantiw@gmail.com>
2022-11-27 17:21:40 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -12,13 +12,13 @@ const App = () => {
   return (
     <div id="main">
       <label htmlFor='text-input'>Text Input:- </label>
-      <input id="text-input" type={'text'} />
+      <input id="text-input" type={'text'} onChange={handleInput} />
 
       <br/>
       <br/>
 
       <label htmlFor='num-input'>Number input</label>
-      <input id="num-input"  type={'number'} />
+      <input id="num-input"  type={'number'} onChange={handleInput} />
       <br/>
     </div>
   )
